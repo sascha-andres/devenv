0a3786177c15571468f038a348061ee9d6142770
Sascha Andres <sascha.andres@outlook.com>
2017-05-25 11:10:00 +0200

    Renamed pull command to setup

--- cmd/setup.go
+++ cmd/setup.go
@@ -26,8 +26,8 @@ import (
 )
 
 // pullCmd represents the pull command
-var pullCmd = &cobra.Command{
-	Use:   "pull",
+var setupCmd = &cobra.Command{
+	Use:   "setup",
 	Short: "Get a project on your harddrive",
 	Long: `Call this to create the directory structure for your multi
 repository project.`,
@@ -55,15 +55,5 @@ repository project.`,
 }
 
 func init() {
-	RootCmd.AddCommand(pullCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// pullCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// pullCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.AddCommand(setupCmd)
 }
